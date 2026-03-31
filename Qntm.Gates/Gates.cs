e52d4f519a7c82fd187c0016a0227eaf0c6b8896
SAVeGH <serkin_av@mail.ru>
2026-03-31 21:09:51 +0300

    Add gates

---
+++ Qntm.Gates/Gates.cs
@@ -0,0 +1,17 @@
+﻿using Qntm.Constants;
+
+namespace Qntm.Gates
+{
+    public static class Gates
+    {
+        public static void H(Quantum q) 
+        {
+            q.Angle = q.Angle + Angles._90degree;
+        }
+
+        public static void X(Quantum q)
+        {
+            q.Angle = q.Angle + Angles._180degree; // negate value
+        }
+    }
+}
