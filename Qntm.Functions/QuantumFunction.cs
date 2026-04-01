b535deadcd1c129d4f704e3ebca766110291a72c
Alexey Serkin <serkin_av@mail.ru>
2026-04-01 18:20:44 +0300

    Save

---
+++ Qntm.Functions/QuantumFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qntm;
+
+namespace Qntm.Functions
+{    
+    public class QuantumFunction
+    {
+        public Func<Quantum, double> qFunction; 
+    }
+}
--- Qntm.Gates/Gates.cs
+++ Qntm.Gates/Gates.cs
@@ -2,16 +2,17 @@
 
 namespace Qntm.Gates
 {
+    // https://sharetechnote.com/html/WebProgramming/Websim_QuantumGate.html
     public static class Gates
     {
         public static void H(Quantum q) 
         {
-            q.Angle = q.Angle + Angles._90degree;
+            q.Angle = Angles._90degree - q.Angle;
         }
 
         public static void X(Quantum q)
         {
-            q.Angle = q.Angle + Angles._180degree; // negate value
+            q.Angle = Angles._180degree - q.Angle; // negate value
         }
     }
 }
